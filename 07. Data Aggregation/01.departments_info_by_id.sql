928012e0974a63933ca10d7aba890d17873fc63c
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-05-15 21:19:27 +0300

    Data Aggregation - Lab
    Date: 15.05.2024

---
+++ 07. Data Aggregation/01.departments_info_by_id.sql
@@ -0,0 +1,10 @@
+SELECT
+	department_id,
+	COUNT(department_id)
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id ASC
+;
---
+++ 07. Data Aggregation/02.departments_info_by_salary.sql
@@ -0,0 +1,10 @@
+SELECT
+	department_id,
+	COUNT(salary)
+FROM
+	employees
+GROUP BY
+	department_id
+ORDER BY
+	department_id ASC
+;
