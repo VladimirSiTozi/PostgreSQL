cdb1f6f86932bfd31f5c10961e6e73df3b632ae0
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-05-26 23:19:15 +0300

    Table Relations - exrs.

---
+++ 10. Table Relations - Exercise/15.countries_without_any_rivers.sql
@@ -0,0 +1,11 @@
+SELECT
+	COUNT(*) AS coutries_without_rivers
+FROM 
+	countries
+LEFT JOIN
+	countries_rivers
+ON
+	countries.country_code = countries_rivers.country_code
+WHERE
+	countries_rivers.country_code IS NULL
+;
