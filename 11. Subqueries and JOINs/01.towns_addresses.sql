e7b7e21fc82503d69f11fea0f2ec9cff4e7e2cfd
VladimirSiTozi <132054352+VladimirSiTozi@users.noreply.github.com>
2024-05-27 23:21:32 +0300

    Subqueries and JOINs - Lab

---
+++ 11. Subqueries and JOINs/01.towns_addresses.sql
@@ -0,0 +1,15 @@
+SELECT
+	t.town_id,
+	t.name,
+	a.address_text
+FROM
+	towns AS t
+		JOIN
+	addresses AS a
+		ON
+	a.town_id = t.town_id
+WHERE
+	t.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY
+	t.town_id ASC,
+	a.address_id ASC
